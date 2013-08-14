1c57592a5b56e4d323bf22f460b668c58a902963
echo511 <ntsiongas@gmail.com>
2013-08-14 20:09:27 +0200

    Fixed Metadata Manipulator removeAttribute method

--- Echo511/TextbookGen/Plugin/Metadata/Manipulator.php
+++ Echo511/TextbookGen/Plugin/Metadata/Manipulator.php
@@ -27,7 +27,6 @@ class Manipulator extends \Nette\Object
 	public function removeAttribute($name, $content)
 	{
 		$pattern = '/@' . $name . ' (.*[^\s])/';
-		$content = $snippet->getContent();
 		return preg_replace($pattern, "", $content);
 	}
 
